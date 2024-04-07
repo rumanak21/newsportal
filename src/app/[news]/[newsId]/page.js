ed0f45e693917a8f98dff71aa264d1cade3190ff
rumanak21 <rumana2k20@gmail.com>
2024-04-08 00:44:48 +0200

    dynamic data load

---
+++ src/app/[news]/[newsId]/page.js
@@ -0,0 +1,51 @@
+import { getSingleNews } from '@/utils/getSingleNews';
+import { Box, Container, Grid, Typography, Avatar } from '@mui/material';
+import Image from 'next/image'
+
+const NewsDetailPage = async ({ params }) => {
+    const { data: news } = await getSingleNews(params.newsId)
+
+    return (
+        <Box className="my-5">
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item lg={6}>
+                        <Image src={news.thumbnail_url} width={800} height={500} alt='news thumbnail' />
+
+                        <Grid container spacing={2} className='my-2'>
+                            <Grid item lg={6}>
+                                <Image src={news.image_url} width={800} height={500} alt='news thumbnail' />
+                            </Grid>
+                            <Grid item lg={6}>
+                                <Image src={news.image_url} width={800} height={500} alt='news thumbnail' />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item lg={6}>
+                        <Typography variant='h5' component="h2">{news.title}</Typography>
+                        <Box sx={{
+                            display:"flex",
+                            gap: 3,
+                            alignItems: "center"
+                        }}>
+                            <Avatar alt="author" src={news.author.img} />
+                            <Typography> By {news.author.name}</Typography>
+                            <Typography> Publish: {news.author.published_date}</Typography>
+                        </Box>
+                        <Typography 
+                        style={{
+                            textAlign: "justify",
+                            whiteSpace: "pre-line",
+                            margin: "10px 0px",
+                            color: 'gray'
+                        }}
+                        > {news.details}</Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+
+        </Box>
+    );
+};
+
+export default NewsDetailPage;
---
+++ src/app/categories/[categoriesId]/page.js
@@ -0,0 +1,61 @@
+import { getCategoryNews } from '@/utils/getCategoryNews';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+const DynamicNewsPage = async ({ params, searchParams }) => {
+
+    const { data } = await getCategoryNews(searchParams.category)
+
+    return (
+        <div className='my-5'>
+            <h1>Total <span className='font-bold'>{searchParams.category}</span> news: {data.length}</h1>
+
+            <Grid className='mt-5' container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {
+                    data.map(news => (
+                        <Grid key={news.id} item xs={6}>
+                            <Link href={`/${news.category.toLowerCase()}/${news._id}`}>
+                                <Card className='my-5'>
+                                    <CardMedia sx={{
+                                        "& img": {
+                                            width: "100%",
+                                            height: "250px",
+                                        }
+                                    }}>
+                                        <Image src={news.thumbnail_url} width={800} height={100} alt='top-news' />
+                                    </CardMedia>
+                                    <CardContent>
+                                        <span className='px-2 my-3 rounded bg-red-500 text-white'>
+                                            {news.category}
+                                        </span>
+                                        <Typography variant='h6' gutterBottom >
+                                            {news.title.length > 30 ? news.title.slice(0, 30) + "..." : news.title}
+                                        </Typography>
+                                        <Typography gutterBottom className='my-2'>
+                                            {news.author.name} - {news.published_date}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {news.details.length > 200 ? news.details.slice(0, 200) + "...." : news.details}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small">Share</Button>
+                                        <Button size="small">Learn More</Button>
+                                    </CardActions>
+                                </Card>
+                            </Link>
+                        </Grid>
+                    ))
+                }
+
+
+
+            </Grid>
+
+        </div>
+    );
+};
+
+export default DynamicNewsPage;
