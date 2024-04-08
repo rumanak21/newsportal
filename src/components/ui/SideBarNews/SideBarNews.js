9cfbb7465d0b32fc517b75e7d51eac6824267be0
rumanak21 <rumana2k20@gmail.com>
2024-04-08 17:21:24 +0200

    sidebar update with dynamic

--- src/components/ui/SideBarNews/SideBarNews.js
+++ src/components/ui/SideBarNews/SideBarNews.js
@@ -5,7 +5,7 @@ import sideBottomImage from "@/assets/side-bottom-img.png"
 import { getAllNews } from '@/utils/getAllNews';
 import Link from 'next/link';
 
-const SideBarNews = async() => {
+const SideBarNews = async () => {
     const { data } = await getAllNews()
     return (
         <>
@@ -16,132 +16,57 @@ const SideBarNews = async() => {
                     </CardMedia>
                     <CardContent>
                         <p className='px-2 my-5 rounded w-[100px] bg-red-500 text-white'>
-                        {data[1].category}
+                            {data[1].category}
                         </p>
                         <Typography gutterBottom variant="h5" component="div">
-                        {data[1].title}
+                            {data[1].title}
                         </Typography>
                         <Typography gutterBottom className='my-3'>
-                        By {data[1].author.name} - {data[1].author.published_date}
+                            By {data[1].author.name} - {data[1].author.published_date}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                        {data[1].details.length > 200 ? data[1].details.slice(0, 200) + "...." : data[1].details}
+                            {data[1].details.length > 200 ? data[1].details.slice(0, 200) + "...." : data[1].details}
                         </Typography>
                     </CardContent>
                     <CardActions>
                         <Link href={`/${data[1].category.toLowerCase()}/${data[1]._id}`}>
-                        <Button size="small">Learn More</Button>
+                            <Button size="small">Learn More</Button>
                         </Link>
                     </CardActions>
 
-                    </Card>
-                
+                </Card>
+
                 <Box>
-                    <Divider className='my-5'></Divider>
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <ButtonBase >
-                                <Image src={sidebarNews} width={100} alt='sidebarNews' />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid item xs={8} sm container>
-                            <Grid item xs container direction="column" >
-                                <Grid item xs>
-                                    <Typography gutterBottom >
-                                        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                                    </Typography>
-                                    <Typography gutterBottom className='my-1'>
-                                        April 03 2024
-                                    </Typography>
-                                </Grid>
+                    {
+                        data.slice(0, 5).map((news) => (
+                            <Link href={`/${news.category.toLowerCase()}/${news._id}`}  key={news._id}>
+                                <Box>
+                                    <Divider className='my-5'></Divider>
+                                    <Grid container spacing={2}>
+                                        <Grid item>
+                                            <ButtonBase >
+                                                <Image src={news.thumbnail_url} width={120} height={100} alt='sidebarNews' />
+                                            </ButtonBase>
+                                        </Grid>
+                                        <Grid item xs={8} sm container>
+                                            <Grid item xs container direction="column" >
+                                                <Grid item xs>
+                                                    <Typography gutterBottom >
+                                                        {news.title}
+                                                    </Typography>
+                                                    <Typography className='text-sm'>
+                                                        {news.author.published_date}
+                                                    </Typography>
+                                                </Grid>
 
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Divider className='my-5'></Divider>
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <ButtonBase >
-                                <Image src={sidebarNews} width={100} alt='sidebarNews' />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid item xs={8} sm container>
-                            <Grid item xs container direction="column" >
-                                <Grid item xs>
-                                    <Typography gutterBottom >
-                                        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                                    </Typography>
-                                    <Typography gutterBottom className='my-1'>
-                                        April 03 2024
-                                    </Typography>
-                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </Link>
+                        ))
+                    }
 
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Divider className='my-5'></Divider>
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <ButtonBase >
-                                <Image src={sidebarNews} width={100} alt='sidebarNews' />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid item xs={8} sm container>
-                            <Grid item xs container direction="column" >
-                                <Grid item xs>
-                                    <Typography gutterBottom >
-                                        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                                    </Typography>
-                                    <Typography gutterBottom className='my-1'>
-                                        April 03 2024
-                                    </Typography>
-                                </Grid>
-
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Divider className='my-5'></Divider>
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <ButtonBase >
-                                <Image src={sidebarNews} width={100} alt='sidebarNews' />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid item xs={8} sm container>
-                            <Grid item xs container direction="column" >
-                                <Grid item xs>
-                                    <Typography gutterBottom >
-                                        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                                    </Typography>
-                                    <Typography gutterBottom className='my-1'>
-                                        April 03 2024
-                                    </Typography>
-                                </Grid>
-
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Divider className='my-5'></Divider>
-                    <Grid container spacing={2}>
-                        <Grid item>
-                            <ButtonBase >
-                                <Image src={sidebarNews} width={100} alt='sidebarNews' />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid item xs={8} sm container>
-                            <Grid item xs container direction="column" >
-                                <Grid item xs>
-                                    <Typography gutterBottom >
-                                        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                                    </Typography>
-                                    <Typography gutterBottom className='my-1'>
-                                        April 03 2024
-                                    </Typography>
-                                </Grid>
-
-                            </Grid>
-                        </Grid>
-                    </Grid>
 
                 </Box>
                 <box>
