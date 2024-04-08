0b2a7a9ff2933d1a25ee483252ee80aa582b8450
rumanak21 <rumana2k20@gmail.com>
2024-04-08 16:10:37 +0200

    Home page dynamic

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -2,27 +2,30 @@ import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Typograph
 import topNews from "@/assets/top-news.png"
 import topNews2 from "@/assets/top-news2.png"
 import Image from 'next/image';
+import { getAllNews } from '@/utils/getAllNews';
 
-const LatestNews = () => {
+const LatestNews = async () => {
+    const { data } = await getAllNews()
+    
     return (
 
         <Box>
             <Card className='my-5'>
                 <CardMedia>
-                    <Image src={topNews} width={800} alt='top-news' />
+                    <Image src={data[0].thumbnail_url} width={800} height={500} alt='top-news' />
                 </CardMedia>
                 <CardContent>
                     <p className='px-2 my-5 rounded w-[100px] bg-red-500 text-white'>
-                        Technology
+                        {data[0].category}
                     </p>
                     <Typography gutterBottom variant="h5" component="div">
-                        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
+                        {data[0].title}
                     </Typography>
                     <Typography gutterBottom className='my-3'>
-                        By Rumana K. - April 03 2024
+                        By {data[0].author.name} - {data[0].author.published_date}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti totam culpa quidem optio facilis, iste voluptates laborum voluptatem minus saepe?
+                        {data[0].details.length > 200 ? data[0].details.slice(0, 200) + "...." : data[0].details}
                     </Typography>
                 </CardContent>
                 <CardActions>
@@ -30,107 +33,44 @@ const LatestNews = () => {
                     <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
-            <Grid className='mt-5'container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                    <Card className='my-5'>
-                        <CardMedia>
-                            <Image src={topNews2} width={800} alt='top-news' />
-                        </CardMedia>
-                        <CardContent>
-                            <p className='px-2 my-5 rounded w-[100px] bg-red-500 text-white'>
-                                Technology
-                            </p>
-                            <Typography gutterBottom >
-                                Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                            </Typography>
-                            <Typography gutterBottom className='my-3'>
-                                By Rumana K. - April 03 2024
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti totam culpa quidem optio facilis, iste voluptates laborum voluptatem minus saepe?
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card className='my-5'>
-                        <CardMedia>
-                            <Image src={topNews2} width={800} alt='top-news' />
-                        </CardMedia>
-                        <CardContent>
-                            <p className='px-2 my-5 rounded w-[100px] bg-red-500 text-white'>
-                                Technology
-                            </p>
-                            <Typography gutterBottom >
-                                Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                            </Typography>
-                            <Typography gutterBottom className='my-3'>
-                                By Rumana K. - April 03 2024
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti totam culpa quidem optio facilis, iste voluptates laborum voluptatem minus saepe?
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card className='my-5'>
-                        <CardMedia>
-                            <Image src={topNews2} width={800} alt='top-news' />
-                        </CardMedia>
-                        <CardContent>
-                            <p className='px-2 my-5 rounded w-[100px] bg-red-500 text-white'>
-                                Technology
-                            </p>
-                            <Typography gutterBottom >
-                                Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                            </Typography>
-                            <Typography gutterBottom className='my-3'>
-                                By Rumana K. - April 03 2024
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti totam culpa quidem optio facilis, iste voluptates laborum voluptatem minus saepe?
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card className='my-5'>
-                        <CardMedia>
-                            <Image src={topNews2} width={800} alt='top-news' />
-                        </CardMedia>
-                        <CardContent>
-                            <p className='px-2 my-5 rounded w-[100px] bg-red-500 text-white'>
-                                Technology
-                            </p>
-                            <Typography gutterBottom >
-                                Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-                            </Typography>
-                            <Typography gutterBottom className='my-3'>
-                                By Rumana K. - April 03 2024
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti totam culpa quidem optio facilis, iste voluptates laborum voluptatem minus saepe?
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
+            <Grid className='mt-5' container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {
+                    data.slice(0, 4).map((news) => (
+                        <Grid key={news._id} item xs={6}>
+                            <Card className='my-5'>
+                                <CardMedia
+                                    sx={{
+                                        "& img": {
+                                            width: "100%",
+                                            height: "250px",
+                                        }
+                                    }}
+                                >
+                                    <Image src={news.thumbnail_url} width={800} height={300} alt='top-news' />
+                                </CardMedia>
+                                <CardContent>
+                                    <span className='px-2 my-3 rounded bg-red-500 text-white'>
+                                        {news.category}
+                                    </span>
+                                    <Typography gutterBottom >
+                                        {news.title}
+                                    </Typography>
+                                    <Typography gutterBottom className='my-3'>
+                                        By {news.author.name} - {news.author.published_date}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {news.details.length > 200 ? news.details.slice(0, 300) + "...." : news.details}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))
+                }
+
 
             </Grid>
         </Box>
